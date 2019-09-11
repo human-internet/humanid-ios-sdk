59d74549d3267021f2b821ffc1401db331d914e7
Fanni Suyuti <fanni@suyuti.com>
2019-09-11 17:01:35 +0700

    Add url extension to get all api url

---
+++ SDK/SDK/Extension/URL.swift
@@ -0,0 +1,40 @@
+//
+//  URL.swift
+//  SDK
+//
+//  Created by fanni suyuti on 11/09/19.
+//  Copyright © 2019 HumanID. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    static var base: URL {
+        return URL(string: "https://humanid.herokuapp.com/mobile/")!
+    }
+    
+    static var loginCheck: URL {
+        return URL(string: "users/login", relativeTo: base)!
+    }
+    
+    static var updatePhone: URL {
+        return URL(string: "users/updatePhone", relativeTo: base)!
+    }
+    
+    static var update: URL {
+        return URL(string: "users", relativeTo: base)!
+    }
+    
+    static var userLogin: URL {
+        return URL(string: "users/login", relativeTo: base)!
+    }
+    
+    static var userRegistration: URL {
+        return URL(string: "users/register", relativeTo: base)!
+    }
+    
+    static var verifyPhone: URL {
+        return URL(string: "users/verifyPhone", relativeTo: base)!
+    }
+    
+}
