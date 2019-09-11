e2bd43ad1bdf5a1d1f7fffa66edc31bb5f96e3ac
Fanni Suyuti <fanni@suyuti.com>
2019-09-11 15:13:54 +0700

    Initial commit, add base project for: 1. SDK 2. Demo App 1 3. Demo App 2

---
+++ DemoAPP1/DemoAPP1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAPP1
+//
+//  Created by fanni suyuti on 11/09/19.
+//  Copyright © 2019 HumanID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
