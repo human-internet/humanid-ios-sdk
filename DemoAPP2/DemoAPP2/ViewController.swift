66619d159ee46deaa6fd278d207d01744ecc5f20
Fanni Suyuti <fanni@suyuti.com>
2019-09-24 10:31:15 +0700

    Test verifyPhone method

--- DemoAPP2/DemoAPP2/ViewController.swift
+++ DemoAPP2/DemoAPP2/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SDK
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        SDK.HumanID.shared.config(appID: "appid", appSecret: "appsecret")
+        
+        
+        SDK.HumanID.shared.verifyPhone(phoneNumber: "08598234", countryCode: "+62") { success, message in
+            print(success)
+            print(message)
+        }
     }
 
 
