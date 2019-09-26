6024348c1cf222a2d8b3142d67a9c69d1fad21e0
Fanni Suyuti <fanni@suyuti.com>
2019-09-26 15:27:37 +0700

    Create struct for responses

---
+++ SDK/HumanID/Model/Responses.swift
@@ -0,0 +1,25 @@
+//
+//  Responses.swift
+//  HumanID
+//
+//  Created by fanni suyuti on 26/09/19.
+//  Copyright © 2019 HumanID. All rights reserved.
+//
+
+import Foundation
+
+public struct BaseResponse<T: Codable>: Codable {
+    let message: String?
+    let data: T?
+}
+
+public struct DefaultResponse: Codable {
+    let message: String
+}
+
+public struct DetailResponse: Codable {
+    let appId: String
+    let hash: String
+    let deviceId: String
+    let notifId: String
+}
