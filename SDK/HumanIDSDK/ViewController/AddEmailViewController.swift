0522a22901319b7416fcece3a81d6d481b3d1b33
Fanni Suyuti <fanni@suyuti.com>
2019-10-16 14:43:44 +0700

    Add Confirm email for existing account screen

--- SDK/HumanIDSDK/ViewController/AddEmailViewController.swift
+++ SDK/HumanIDSDK/ViewController/AddEmailViewController.swift
@@ -8,25 +8,46 @@
 
 import UIKit
 
+public enum ConfirmEmailType {
+    case newAccount
+    case confirmAccount
+}
+
 class AddEmailViewController: UIViewController {
 
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var confirmTitleLabel: UILabel!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var saveButton: UIButton!
+    var screenType: ConfirmEmailType = .newAccount
     
-    convenience init(clientImage: UIImage, clientName: String) {
+    convenience init(type: ConfirmEmailType) {
         self.init(nibName: "AddEmailViewController", bundle: Bundle(identifier: "org.humanid.HumanIDSDK"))
         
-        
+        self.screenType = type
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        configureView()
+    }
+    
+    func configureView() {
         saveButton.isEnabled = false
         saveButton.backgroundColor = UIColor(red: 84.7/100, green: 84.7/100, blue: 84.7/100, alpha: 1.0)
         saveButton.layer.cornerRadius = 5
         saveButton.clipsToBounds = true
-
+        
+        switch screenType {
+        case .newAccount:
+            confirmTitleLabel.text = "For Your Security:"
+            descriptionLabel.text = "Please enter your email address to recover access in case you lose your phone number. Ensure to enter an address you can access and remember! Only a non-readable encryption will be saved!"
+        case .confirmAccount:
+            confirmTitleLabel.text = "Last Step - For Your Security:"
+            descriptionLabel.text = "To make sure we got the right account, please type your email address: "
+        }
     }
     
     @IBAction func emailFieldEditingChanged(_ sender: UITextField) {
