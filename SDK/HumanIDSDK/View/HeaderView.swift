e0ce494fb5e9ded9bf391c7ba140cdea266f3db6
Fanni Suyuti <fanni@suyuti.com>
2019-10-16 08:56:25 +0700

    Refactor header view

--- SDK/HumanIDSDK/View/HeaderView.swift
+++ SDK/HumanIDSDK/View/HeaderView.swift
@@ -9,57 +9,30 @@
 import Foundation
 
 class HeaderView: UIView {
-    
+    @IBOutlet var contentView: UIView!
     override init(frame: CGRect) {
-        super.init(frame: UIScreen.main.bounds)
-        setupView()
+        super.init(frame: frame)
+        
+        customInit()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        setupView()
+        
+        customInit()
     }
     
-    func setupView() {
-        let fingerprintImage = UIImage(named: "HumanIDLogoTransparent")
-        let humanIDLogoImage = UIImage(named: "HumanIDLogoTextTransparent")
+    func customInit() {
+        guard let humanIDBundle = Bundle.humanID else {
+            print("bundle not found")
+            return
+            
+        }
         
-        let fingerView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
-        fingerView.image = fingerprintImage
+        humanIDBundle.loadNibNamed("HeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         
-        let logoView = UIImageView(frame: CGRect(x: 0, y: 0, width: 73, height: 15))
-        logoView.image = humanIDLogoImage
-        
-        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 115, height: 30))
-        let bottomLineView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: 1.0))
-        bottomLineView.backgroundColor = .gray
-        
-        containerView.addSubview(fingerView)
-        containerView.addSubview(logoView)
-        addSubview(containerView)
-        addSubview(bottomLineView)
-        
-        
-        
-        fingerView.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
-        fingerView.widthAnchor.constraint(equalToConstant: 30.0).isActive = true
-        fingerView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
-        fingerView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
-        
-        logoView.heightAnchor.constraint(equalToConstant: 15.0).isActive = true
-        logoView.widthAnchor.constraint(equalToConstant: 73.0).isActive = true
-        logoView.leadingAnchor.constraint(equalTo: fingerView.trailingAnchor, constant: 12.0).isActive = true
-        logoView.centerYAnchor.constraint(equalTo: fingerView.centerYAnchor).isActive = true
-        logoView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
-        
-        logoView.anchorTo(top: nil, leading: nil, bottom: nil, trailing: trailingAnchor)
-        
-        containerView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        containerView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        
-        bottomLineView.anchorTo(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
-        bottomLineView.heightAnchor.constraint(equalToConstant: 1.0).isActive = true
-        
-        heightAnchor.constraint(equalToConstant: 50.0).isActive = true
     }
 }
--- SDK/HumanIDSDK/ViewController/ConfirmEmailViewController.swift
+++ SDK/HumanIDSDK/ViewController/ConfirmEmailViewController.swift
@@ -10,29 +10,14 @@ import UIKit
 
 class ConfirmEmailViewController: UIViewController {
 
-    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var headerView: HeaderView!
     
     convenience init() {
-        self.init(nibName: "ConfirmEmailViewController", bundle: Bundle(identifier: "org.humanid.HumanIDSDK"))
+        self.init(nibName: "ConfirmEmailViewController", bundle: Bundle.humanID)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let contentHeaderView = HeaderView(frame: headerView.frame)
-        headerView.addSubview(contentHeaderView)
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
