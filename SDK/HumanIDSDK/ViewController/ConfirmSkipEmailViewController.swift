58eb77c63777e049e01bc1fad4b8efb39ce14884
Fanni Suyuti <fanni@suyuti.com>
2019-10-16 20:29:03 +0700

    confirm skipping email screen

---
+++ SDK/HumanIDSDK/ViewController/ConfirmSkipEmailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ConfirmSkipEmailViewController.swift
+//  HumanIDSDK
+//
+//  Created by fanni suyuti on 16/10/19.
+//  Copyright © 2019 HumanID. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmSkipEmailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func skipButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func learnMoreButtonAction(_ sender: Any) {
+    }
+}
