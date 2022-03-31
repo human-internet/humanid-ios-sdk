a3196f40c6bdc3ebb2558c63ca2e9547d49ecc2a
Budi Oktaviyan <budioktaviyans@gmail.com>
2022-04-01 01:54:25 +0700

    Update base URL

--- HumanIDSDK/Classes/Extensions/String+Key.swift
+++ HumanIDSDK/Classes/Extensions/String+Key.swift
@@ -1,8 +1,8 @@
 internal extension String {
 
     static var webLoginPath: String {
-        let version = "v0.0.3"
-        return HumanIDSDK.isStaging ? "/mobile/users/web-login" : "\(version)/mobile/users/web-login"
+        let version = "v1"
+        return "\(version)/mobile/users/web-login"
     }
 
     func getQueryParameter(from key: String) -> String? {
--- HumanIDSDK/Classes/Extensions/URL.swift
+++ HumanIDSDK/Classes/Extensions/URL.swift
@@ -1,8 +1,7 @@
 internal extension URL {
 
     static var base: URL {
-        // FIXME: - Staging url will change into https://stg-core.human-id.org
-        let path = HumanIDSDK.isStaging ? "https://core.human-id.org/staging" : "https://core.human-id.org"
+        let path = HumanIDSDK.isStaging ? "https://s-api.human-id.org" : "https://api.human-id.org"
         return URL(string: path)!
     }
 }
